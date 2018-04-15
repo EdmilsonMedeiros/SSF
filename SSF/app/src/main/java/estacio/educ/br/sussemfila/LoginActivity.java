692dc46d4304150079c92ca5c95ec764a9b876db
EdmilsonMedeiros <edmilsonnmedeiros@gmail.com>
2018-04-14 22:58:05 -0300

    Atualizações...

--- SSF/app/src/main/java/estacio/educ/br/sussemfila/LoginActivity.java
+++ SSF/app/src/main/java/estacio/educ/br/sussemfila/LoginActivity.java
@@ -2,12 +2,29 @@ package estacio.educ.br.sussemfila;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    //Atri
+    protected EditText editTextCpfSus;
+    protected EditText editTextPass;
+    protected Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Inst
+        editTextCpfSus = findViewById(R.id.editTextCpfSus);
+        editTextPass = findViewById(R.id.editTextPass);
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+    }
+    //Met
+    public void logar(View v){
+
     }
 }
