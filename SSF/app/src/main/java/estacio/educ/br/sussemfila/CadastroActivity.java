c99331df21497d3cc96f262fe064029bf18a30d5
Natanael Xavier <natanaelxavierr@gmail.com>
2018-04-19 21:32:17 -0300

    Conexão com a base

--- SSF/app/src/main/java/estacio/educ/br/sussemfila/CadastroActivity.java
+++ SSF/app/src/main/java/estacio/educ/br/sussemfila/CadastroActivity.java
@@ -2,12 +2,57 @@ package estacio.educ.br.sussemfila;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import org.json.JSONException;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView nome;
+    private AutoCompleteTextView endereco;
+    private AutoCompleteTextView email;
+    private AutoCompleteTextView numerocpf;
+    private AutoCompleteTextView numerosus;
+    private AutoCompleteTextView pass1;
+    private AutoCompleteTextView pass2;
+    private Button botaocadastrar;
+    private Button botaocancelar;
+    private String pass;
+    private boolean pass_confirm;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        AutoCompleteTextView nome = (AutoCompleteTextView) findViewById(R.id.editTextNome);
+        AutoCompleteTextView endereco = (AutoCompleteTextView) findViewById(R.id.editTextEndereço);
+        AutoCompleteTextView email = (AutoCompleteTextView) findViewById(R.id.editTextEmail);
+        AutoCompleteTextView numerocpf = (AutoCompleteTextView) findViewById(R.id.editTextCpf);
+        AutoCompleteTextView numerosus = (AutoCompleteTextView) findViewById(R.id.editTextSus);
+        AutoCompleteTextView pass1 = (AutoCompleteTextView) findViewById(R.id.editTextPass1);
+        AutoCompleteTextView pass2 = (AutoCompleteTextView) findViewById(R.id.editTextPass2);
+        Button botaocadastrar = (Button) findViewById(R.id.buttonCadastrar);
+        Button botaocancelar = (Button) findViewById(R.id.buttonCancelar);
     }
+
 }
--- SSF/app/src/main/java/estacio/educ/br/sussemfila/LoginActivity.java
+++ SSF/app/src/main/java/estacio/educ/br/sussemfila/LoginActivity.java
@@ -1,16 +1,30 @@
 package estacio.educ.br.sussemfila;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class LoginActivity extends AppCompatActivity {
     //Atri
-    protected EditText editTextCpfSus;
+    protected EditText editTextCpf;
     protected EditText editTextPass;
     protected Button btnEntrar;
+    protected TextView cadastrese;
+    protected TextView titulo;
+
+    String url= "";
+    String parametros = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +32,55 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         //Inst
-        editTextCpfSus = findViewById(R.id.editTextCpfSus);
+        editTextCpf = findViewById(R.id.editTextCpf);
         editTextPass = findViewById(R.id.editTextPass);
         btnEntrar = findViewById(R.id.btnEntrar);
+        cadastrese = findViewById(R.id.cadastrese);
+        titulo = findViewById(R.id.textViewLogin);
+
+        cadastrese.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent abrecadastro = new Intent(LoginActivity.this, CadastroActivity.class);
+                startActivity(abrecadastro);
+            }
+        });
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ConnectivityManager connMgr = (ConnectivityManager)
+                        getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+                if (networkInfo != null && networkInfo.isConnected()) {
+                    String cpf = editTextCpf.getText().toString();
+                    String senha = editTextPass.getText().toString();
+                      if(cpf.isEmpty() || senha.isEmpty()){
+                          Toast.makeText(getApplicationContext(), "CAMPOS VAZIOS!", Toast.LENGTH_LONG).show();
+                      }else {
+                          url = "https://sussemfila.000webhostapp.com/login.php";
+                          parametros = "cpf="+cpf+"&senha="+senha;
+                          new SolicitaDados().execute(url);
+                      }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Sem Conexão com a Internet", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
-    //Met
-    public void logar(View v){
 
-        setContentView(R.layout.activity_main);
-    }
+    private class SolicitaDados extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            return conexao.postDados(urls[0], parametros);
+        }
 
-    public void cadastrar(View v){
-
-        setContentView(R.layout.activity_cadastro);
+        @Override
+        protected void onPostExecute(String result) {
+            titulo.setText(result);
+        }
     }
 }
