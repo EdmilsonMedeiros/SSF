1c6cad689a238272a875a3c3724e049243ce7aac
EdmilsonMedeiros <edmilsonnmedeiros@gmail.com>
2018-06-18 18:23:35 -0300

    Alterações finales

--- SusSemFila/app/src/main/java/br/com/geekstorm/sussemfila/MainActivity.java
+++ SusSemFila/app/src/main/java/br/com/geekstorm/sussemfila/MainActivity.java
@@ -2,17 +2,24 @@ package br.com.geekstorm.sussemfila;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "";
     //Widgets do Layout
     ImageView agendamento, consAgendadas, mainBthistorico, Main_btconfiguracao, main_perfil;
     TextView nomeusuario, cpfusuario, btsair;
@@ -35,15 +42,15 @@ public class MainActivity extends AppCompatActivity {
         consAgendadas = findViewById(R.id.Main_btconsultaagendamento);
         mainBthistorico = findViewById(R.id.mainBthistorico);
         Main_btconfiguracao = findViewById(R.id.Main_btconfiguracao);
-        main_perfil = findViewById(R.id.main_perfil);
 
+
+        main_perfil = (ImageView) findViewById(R.id.main_perfil);
         main_perfil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
             }
         });
-
         this.sessao = new UsuarioSessao(getApplicationContext());
 
         //Abrindo consultas agendadas
@@ -151,14 +158,15 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
-    /*
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             main_perfil.setImageBitmap(imageBitmap);
         }
-    }*/
+    }
     //Função sair e deletar sessão
     private void logoff(){
         finish();
